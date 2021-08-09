1df1c606df3ae195cc7fbabd97a5b6594eba29e1
SilviaDam91 <silvia.pariota@gmail.com>
2021-08-09 19:38:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import questionDog from "./questionDog.jpg";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container"> Container Bootstrap </div>
+
+        <img
+          src={questionDog}
+          alt="questionDog"
+          className="img-fluid questionDog"
+        />
       </header>
     </div>
   );
