fdd5482f3144be174f12cba1c1d7ae86bcbca73c
SilviaDam91 <silvia.pariota@gmail.com>
2021-08-09 19:45:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container"> </div>
+        <div className="container"> Dictionary </div>
 
         <img
           src={questionDog}
